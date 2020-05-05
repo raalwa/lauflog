025e7661c8facf4c72c73f8d3f0a180f74434d23
Raphael Wagner <raphael-wagner@gmx.de>
2020-05-05 18:46:21 +0200

    [ADDED] xmlhttp request for index.html

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
 const AppServer = require("./lib/AppServer.js");
 
-var formidable = require('formidable');
-var mv = require('mv');
+var formidable = require("formidable");
+var mv = require("mv");
 var server;
+var fs = require("fs");
 
 /**
  * Starts webserver to serve files from "/app" folder
@@ -17,19 +18,35 @@ function init() {
   server.start(appPort);
   server.app.post("/file-upload", function(req, res) {
     console.log("received");
-    var form = new formidable.IncomingForm();
+    let form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var newpath = __dirname + '/app/uploads/' + files.filetoupload.name;
+      var oldpath = files.filetoupload.path,
+        newpath = __dirname + "/app/uploads/" + files.filetoupload.name;
       mv(oldpath, newpath, function(err) {
         if (err) {
           console.log(err);
         }
       })
-      res.write('File uploaded and moved!');
+      res.write("File uploaded and moved!");
       res.end();
     });
   });
+  server.app.get("/gpx", function(req, res) {
+    fs.readdir(__dirname + "/app/uploads/", function(err, files) {
+      if (err) {
+        return console.log(err);
+      }
+      let response = [];
+      files.forEach(function(file) {
+        // formData.append(file.basepath);
+        // console.log(file);
+        response.push("uploads/" + file);
+        // res.sendFile("/app/uploads/" + file, { root: __dirname });
+        console.log("uploads/" + file);
+      });
+      res.send(JSON.stringify(response));
+    })
+  })
 }
 
 init();
